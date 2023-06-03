37477a0f0d111481a1d01c89d311d2aab3655ab8
Jason-Tysl <jktysl@gmail.com>
2023-06-03 15:26:04 -0400

    implemented expr and term functionality

--- Expr.java
+++ Expr.java
@@ -3,8 +3,29 @@ public class Expr {
     // Add fields for each child the Expr node could have
     Term term;
     Expr expr;
+    expressionType exprType;
+
+    enum expressionType {
+        BASE, ADD, SUBTRACT
+    }
 
     void parse(Scanner scanner) {
+        term = new Term();
+        term.parse(scanner);
+
+        Core currentToken = scanner.currentToken();
+        if (currentToken == Core.ADD) {
+            exprType = expressionType.ADD;
+        } else if (currentToken == Core.SUBTRACT) {
+            exprType = expressionType.SUBTRACT;
+        } else {
+            exprType = expressionType.BASE;
+            return;
+        }
+
+        scanner.nextToken();
+        expr = new Expr();
+        expr.parse(scanner);
 
     }
 
--- Term.java
+++ Term.java
@@ -3,8 +3,29 @@ public class Term {
     // Add fields for each child the Term node could have
     Factor factor;
     Term term;
+    termType trmType;
+
+    enum termType {
+        BASE, MULTIPLY, DIVIDE
+    }
 
     void parse(Scanner scanner) {
+        factor = new Factor();
+        factor.parse(scanner);
+
+        Core currentToken = scanner.currentToken();
+        if (currentToken == Core.MULTIPLY) {
+            trmType = termType.MULTIPLY;
+        } else if (currentToken == Core.DIVIDE) {
+            trmType = termType.DIVIDE;
+        } else {
+            trmType = termType.BASE;
+            return;
+        }
+
+        scanner.nextToken();
+        term = new Term();
+        term.parse(scanner);
 
     }
 
