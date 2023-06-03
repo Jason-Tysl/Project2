4bb46cd33881cd00f024b9854d845e3f4dad6579
Jason-Tysl <jktysl@gmail.com>
2023-06-02 20:08:23 -0400

    added if functionality

--- If.java
+++ If.java
@@ -5,7 +5,31 @@ public class If {
     StmtSeq stmtSeq;
 
     void parse(Scanner scanner) {
+        scanner.nextToken();
+        cond = new Cond();
+        cond.parse(scanner);
 
+        Core currentToken = scanner.currentToken();
+        if (currentToken == Core.THEN) {
+            scanner.nextToken();
+            stmtSeq = new StmtSeq();
+            stmtSeq.parse(scanner);
+        }
+
+        currentToken = scanner.currentToken();
+
+        if (currentToken == Core.END) {
+            scanner.nextToken();
+        } else if (currentToken == Core.ELSE) {
+            scanner.nextToken();
+            stmtSeq = new StmtSeq();
+            stmtSeq.parse(scanner);
+        } else {
+            System.out.println("ERROR: Expected \"END\" or \"ELSE\" token, got: " + currentToken.toString());
+            System.exit(0);
+        }
+
+        
     }
 
     void semantic() {
